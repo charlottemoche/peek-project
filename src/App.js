b3c51ac4390bebc304e70a57aad585579b8da3bd
Charlotte Moche <69945837+charlottemoche@users.noreply.github.com>
2022-10-28 10:53:29 -0400

    cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import axios from 'axios'
 import Activities from './activities_page_1.json'
+import Card from 'react-bootstrap/Card';
+
 
 function App() {
 
   const activityListings = Activities.data.activities.results.entries;
+
+  // const [activities, setActivities] = useState([]);
+  // const [loading, setLoading] = useState(false)
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [activitiesPerPage, setActivitiesPerPage] = useState(20);
+
+  // useEffect(() => {
+  //   const fetchActivities = async () => {
+  //     setLoading(true);
+  //     const response = await axios.get('localhost:3000/activities_page_1.json');
+  //     setActivities(response.data);
+  //     setLoading(false);
+  //   }
+  //   fetchActivities();
+  // });
+
   
   return (
     <div className="activity-container">
@@ -12,11 +31,14 @@ function App() {
           activityListings.map(activity => {
             return ( 
               <>
-              <p key={activity.name}>
-                {activity.name}
-              </p>
-              <img key={activity.images} className="img-card" src={activity.images[0]}></img>
+               <Card style={{ width: '18rem' }}>
+               <Card.Img variant="top" src={activity.images[0]} />
+               <Card.Body>
+               <Card.Title>{activity.name}</Card.Title>
+               </Card.Body>
+               </Card>
               </>
+              
             )
           })
         }
