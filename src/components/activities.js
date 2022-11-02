3788d5e168a22b5681cbb7a0c26230522dfa2c08
Charlotte Moche <69945837+charlottemoche@users.noreply.github.com>
2022-11-02 14:54:46 -0400

    activity detail

--- src/components/activities.js
+++ src/components/activities.js
@@ -33,7 +33,55 @@ export const Activities = ({ activityListings, loading }) => {
       </Carousel>
       </div>
 
-    <div className="activity-detail">
+
+
+
+
+      {/* <div className="activity-detail">
+        <div className="activity-detail-container">
+        <div className="activity-detail-1">
+          <img src="/peek-logo.png"></img>
+          <h1>TITLE</h1>
+          <i className="fa fa-star"></i>5
+          <p className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="info-box">
+            <ul>Lorem ipsum dolor sit amet</ul>
+            <ul>Lorem ipsum dolor sit amet</ul>
+            <ul>Lorem ipsum dolor sit amet</ul>
+          </div>
+          <div className="details">
+            <h2>
+              Highlights
+            </h2>
+            <p>
+              <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              </li>
+              <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              </li>
+              <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              </li>
+            </p>
+            <h2>
+              What's Included
+            </h2>
+            </div>
+          <img src="/map-placeholder.png"></img>
+        </div>
+
+        <div className="activity-detail-2">
+          <img className="calendar" src="/calendar-placeholder.png"></img>
+        </div>
+        </div>
+      </div>  */}
+
+
+
+{/* 
       <img src="/peek-logo.png"></img>
       <h1>TITLE</h1>
       <i className="fa fa-star"></i>5
@@ -63,8 +111,14 @@ export const Activities = ({ activityListings, loading }) => {
       <h2>
         What's Included
       </h2>
-      </div>
-    </div>
+      </div>*/}
+    
+    
+
+
+
+
+
 
     <div className="categories">
         <span>
