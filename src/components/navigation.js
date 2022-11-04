cc4a4ebcdf5a765644453ca7b3b28786a272d670
Charlotte Moche <69945837+charlottemoche@users.noreply.github.com>
2022-11-04 13:34:43 -0400

    navbar logo linkto

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,7 +6,9 @@ export const Navigation = () => {
   return (
     <>
     <div className="navbar-static">
+      <Link to="/">
       <img className="logo" src="./peek-logo.png" alt="" />
+      </Link>
       <p className="right-nav">Products</p>
       <p className="right-nav-2">Log In / Sign Up</p>
     </div>
